a83a47f81effc9c00677bc3b7b85637b9861e64d
Isaac Plunkett <plunkett5@gmail.com>
2017-01-14 12:19:31 -0700

    mouse eats cheese

---
+++ mouseCheese/Assets/Scripts/cheese.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cheese : MonoBehaviour
+{
+
+	void OnTriggerEnter(Collider other)
+	{
+		gameObject.SetActive(false);
+	}
+}
