75c483f8da5eea09a880892098e637421e0f3b24
Isaac Plunkett <plunkett5@gmail.com>
2017-01-14 13:30:20 -0700

    added a cheese counter

---
+++ mouseCheese/Assets/Scripts/CheeseCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseCounter : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update()
+	{
+		if(GameObject.FindGameObjectsWithTag("Cheese").Length == 0)
+		{
+			print("You Win");
+		}
+	}
+}
